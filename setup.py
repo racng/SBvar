29e2e8dccfcb0b32c5b876c9e8ef05166df488ae
racng <rachelng323@gmail.com>
2021-12-10 06:33:40 -0800

    switch to miniconda for installing requirements

--- setup.py
+++ setup.py
@@ -9,10 +9,13 @@ setup(
     url="https://github.com/racng/SBvar",
     description="Varying parameter analysis for SBML models",
     license="MIT", 
+    python_requires='>=3.7',
     classifiers=[
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+
         'Operating System :: OS Independent',
     ],
     install_requires=[
